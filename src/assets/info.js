1a31b89b16d41c2b0280d4bc3d88240e425a1e9d
singhsahib <ssubhi.24@gmail.com>
2024-10-27 15:37:55 -0700

    added poster.

---
+++ src/assets/info.js
@@ -0,0 +1,16 @@
+export const info = {
+  name: "Sahib Singh",
+  edu: {
+    school: "Delhi Public School",
+    bachelor: "University of Northern British Columbia",
+  },
+  work: {
+    mobDev: "Worked in computer science researcj labto create some ",
+  },
+  interests: [
+    "Black Holes",
+    "Topological Phases of Matter",
+    "Quantum Spin Liquids",
+    "Unconventional Superconductors",
+  ],
+};
